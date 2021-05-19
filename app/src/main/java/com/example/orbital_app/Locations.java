a104fee3dc78307d1f637ff9ffe7746f117661f4
Justin <justineezj0@gmail.com>
2021-05-19 23:11:46 +0800

    Locations class (to test recycler view, not final)

--- app/src/main/java/com/example/orbital_app/Locations.java
+++ app/src/main/java/com/example/orbital_app/Locations.java
@@ -2,7 +2,7 @@ package com.example.orbital_app;
 
 public class Locations {
     private String name;
-    private String imageUrl;
+    private final String imageUrl;
 
     public Locations(String name, String imageUrl) {
         this.name = name;
@@ -21,15 +21,4 @@ public class Locations {
         this.name = name;
     }
 
-    public void setImageUrl(String imageUrl) {
-        this.imageUrl = imageUrl;
-    }
-
-    @Override
-    public String toString() {
-        return "Locations{" +
-                "name='" + name + '\'' +
-                ", imageUrl='" + imageUrl + '\'' +
-                '}';
-    }
 }
