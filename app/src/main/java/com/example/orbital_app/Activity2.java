fff05b37c6533f1284d69bec1eacb5fd2c334910
Justin <justineezj0@gmail.com>
2021-05-21 22:10:52 +0800

    changed code to adapt to usage of scrollview, and removed adapter class used for recycler view previously

--- app/src/main/java/com/example/orbital_app/Activity2.java
+++ app/src/main/java/com/example/orbital_app/Activity2.java
@@ -6,26 +6,36 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.ScrollView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
 
 public class Activity2 extends AppCompatActivity {
-
-    private RecyclerView speclocationRecView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_2);
 
-        speclocationRecView = findViewById(R.id.speclocationRV);
-
-        SpecificLocRVAdapter adapter = new SpecificLocRVAdapter(this);
-
         Bundle bundle = getIntent().getExtras();
         Locations location = new Locations(bundle.getString("location"), bundle.getString("image"));
 
-        adapter.setLocations(location);
-        speclocationRecView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
-        speclocationRecView.setItemAnimator(new DefaultItemAnimator());
-        speclocationRecView.setAdapter(adapter);
+        TextView tv = findViewById(R.id.txtName);
+        tv.setText(location.getName());
+
+        ImageView iv = findViewById(R.id.image);
+
+        Glide.with(Activity2.this)
+                .asBitmap()
+                .load(location.getImageUrl())
+                .into(iv);
+
+
     }
 }
