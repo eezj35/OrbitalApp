70d02fe4eed6db1951d3d71e380276bad9463f54
Justin <justineezj0@gmail.com>
2021-06-02 23:50:46 +0800

    changed leave review card to a button instead

--- app/src/main/java/com/example/orbital_app/ReviewActivity.java
+++ app/src/main/java/com/example/orbital_app/ReviewActivity.java
@@ -8,6 +8,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import java.util.ArrayList;
 
@@ -30,7 +31,7 @@ public class ReviewActivity extends AppCompatActivity {
         rv.setLayoutManager(new LinearLayoutManager(this));
         rv.setAdapter(adapter);
 
-        CardView leaveReview = findViewById(R.id.leaveReview);
+        Button leaveReview = findViewById(R.id.leaveReview);
         leaveReview.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
