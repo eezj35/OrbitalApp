48eb9045e462b13ee1f88e2174c642b34e492856
Justin <justineezj0@gmail.com>
2021-06-14 16:58:23 +0800

    user's preferred activities are now saved to realtime database

--- app/src/main/java/com/example/orbital_app/PrefForm.java
+++ app/src/main/java/com/example/orbital_app/PrefForm.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Toast;
@@ -18,10 +19,18 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
+
 public class PrefForm extends AppCompatActivity {
 
     RadioGroup radioGroup;
     RadioButton radioButton;
+
+    CheckBox cb1;
+    CheckBox cb2;
+    CheckBox cb3;
+    CheckBox cb4;
+    CheckBox cb5;
 
     private FirebaseDatabase rtdb = FirebaseDatabase.getInstance();
     DatabaseReference prefRef;
@@ -38,6 +47,22 @@ public class PrefForm extends AppCompatActivity {
 
         radioGroup = findViewById(R.id.radioGroup);
 
+        cb1 = findViewById(R.id.cb1);
+        cb2 = findViewById(R.id.cb2);
+        cb3 = findViewById(R.id.cb3);
+        cb4 = findViewById(R.id.cb4);
+        cb5 = findViewById(R.id.cb5);
+
+        ArrayList<CheckBox> cbList = new ArrayList<>();
+
+        cbList.add(cb1);
+        cbList.add(cb2);
+        cbList.add(cb3);
+        cbList.add(cb4);
+        cbList.add(cb5);
+
+
+
         Button btnFinish = findViewById(R.id.btnFinish);
         btnFinish.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -49,6 +74,13 @@ public class PrefForm extends AppCompatActivity {
                     @Override
                     public void onDataChange(@NonNull DataSnapshot snapshot) {
                         prefRef.child("house").setValue(radioButton.getText().toString());
+                        int i=1;
+                        for(CheckBox cb: cbList){
+                            if(cb.isChecked()){
+                                prefRef.child("activities").child("activity"+i).setValue(cb.getText());
+                                i++;
+                            }
+                        }
                     }
 
                     @Override
